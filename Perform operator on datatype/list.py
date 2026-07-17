44bb8d88ca737b82ee2df9942fc3d2b113dea458
Aditya24-rathore <ra4402538@gmail.com>
2026-07-17 23:02:47 +0530

    list operator update

--- Perform operator on datatype/list.py
+++ Perform operator on datatype/list.py
@@ -1,2 +1,29 @@
+#1. Assignment operator
+# a. Addition operator
+l1=['java','python','cpp']
+l2=['js','html','react']
+l3=l1+l2
+print(l3) #['java', 'python', 'cpp', 'js', 'html', 'react']
+
+l1=[1,2,3]
+l2=[3,4,5]
+l3=l1+l2
+print(l3) #[1, 2, 3, 3, 4, 5]
+
+l1=[1,2,'java']
+l2=['java','1']
+l3=l1+l2
+print(l3) #[1, 2, 'java', 'java', '1']
 
 
+#b. subtraction operator:-SO basically subtraction operator cant perform on list
+l1=['java','python','cpp']
+l2=['js','html','react']
+l3=l1-l2
+print(l3) #unsupported operand type(s) for -: 'list' and 'list'
+
+#c. multiplication operator:-SO basically multiplication operator cant perform on list
+l1=['java','python','cpp']
+l2=['js','html','react']
+l3=l1*l2
+print(l3) #can't multiply sequence by non-int of type 'list'
