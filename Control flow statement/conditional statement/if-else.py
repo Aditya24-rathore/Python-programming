073a41fcce6047e107d1cac40ac54830298187e3
Aditya24-rathore <ra4402538@gmail.com>
2026-07-27 23:14:21 +0530

    update

---
+++ Control flow statement/conditional statement/if-else.py
@@ -0,0 +1,14 @@
+# Coditional Statement:- 
+#  1. if :- Independent Statement and work on single single statement
+# 2. if-elif :-Dependent statement and work on multiple statement
+# 3. if-elif else:- dependent
+# 4. else:- Dependent on if-elif
+
+# 1. if statement:- 
+x=10
+y=10
+if x==y:
+    print("Both value are equal")
+if x==y:
+    print('xyz')
+print('abc') #These is not a part of condition statement these will be print
